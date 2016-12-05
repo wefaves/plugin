c324ca4b2322bb01df71d378357db7fc56917c86
Kadium <charlesh.tau@gmail.com>
2016-12-05 13:32:11 -0500

    beginning history

---
+++ public/js/history.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $('#allHistory').click(tabsHistory);
+
+  function tabsHistory() {
+    chrome.tabs.create({url: 'chrome://history'});
+  }
+
+  var date = new Date();
+  today = new Date(date.getFullYear(),date.getMonth(),date.getDate(),0,0,0,0);
+  getHistory(today, 15);
+  function getHistory(day, nb){
+    var dateStart = new Date(day.getFullYear(),day.getMonth(),day.getDate(),0,0,0,0);
+    var dateEnd = new Date(day.getFullYear(),day.getMonth(),day.getDate(),23,59,59);
+    var query = {
+      text: '',
+      startTime: dateStart.getTime(),
+      endTime: dateEnd.getTime(),
+      maxResults: nb
+    };
+    loading = true;
+    chrome.history.search(query, function(results){
+      console.log(results);
+    });
+  };
+});
--- public/js/process.js
+++ public/js/process.js
@@ -5,6 +5,10 @@ $('#clientAccount').click(function() {
   });
 });
 
+$('#btnHistory').click(function() {
+  window.location.href="history.html";
+});
+
 
 $('#synchro').click(function() {
   sendBookmarks();
@@ -14,19 +18,19 @@ $('#synchro').click(function() {
 function  sendBookmarks()
 {
   var bookmarkTreeNodes = chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
-      console.log(bookmarkTreeNodes);
-      $.ajax({
-        url: './php/test.php',
-        type: 'POST',
-        data: JSON.stringify(bookmarkTreeNodes),
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-        async: false,
-        success: function(msg) {
-            console.log(msg);
-        }
-      });
+    console.log(bookmarkTreeNodes);
+    $.ajax({
+      url: './php/test.php',
+      type: 'POST',
+      data: JSON.stringify(bookmarkTreeNodes),
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      async: false,
+      success: function(msg) {
+        console.log(msg);
+      }
     });
+  });
 }
 
 function  getBookmarks()
