5f38457aa24c28f459e003e47eb591ff9d3efa19
Kadium <charlesh.tau@gmail.com>
2016-12-07 05:04:08 -0500

    display history

--- public/js/history.js
+++ public/js/history.js
@@ -1,29 +1,37 @@
 $(document).ready(function() {
   $('#allHistory').click(tabsHistory);
-
-  function tabsHistory() {
-    chrome.tabs.create({url: 'chrome://history'});
-  }
+  getHistory(15);
 
   $('#btnFaves').click(function() {
     window.location.href="faves.html";
   });
 
-  var date = new Date();
-  today = new Date(date.getFullYear(),date.getMonth(),date.getDate(),0,0,0,0);
-  getHistory(today, 15);
-  function getHistory(day, nb){
-    var dateStart = new Date(day.getFullYear(),day.getMonth(),day.getDate(),0,0,0,0);
-    var dateEnd = new Date(day.getFullYear(),day.getMonth(),day.getDate(),23,59,59);
+  function tabsHistory() {
+    chrome.tabs.create({url: 'chrome://history'});
+  }
+
+  function getHistory(nb){
     var query = {
       text: '',
-      startTime: dateStart.getTime(),
-      endTime: dateEnd.getTime(),
       maxResults: nb
     };
     loading = true;
     chrome.history.search(query, function(results){
       console.log(results);
+      displayHistory(results);
     });
+  };
+
+  function displayHistory(results) {
+    var data = "";
+    for (var i = 0; i < results.length; i++) {
+      //console.log(results[i].id);
+      data += '<div class="urlLink" id="' + results[i].id + '">';
+      data += strftime('%T', new Date(results[i].lastVisitTime)) + ' ';
+      data += '<img class="favLink" src="chrome://favicon/size/16@1x/' + results[i].url + '" alt="">';
+      data += ' <a target="_blank" href="' + results[i].url + '">' + results[i].title + '</a>';
+      data +='</div>';
+    }
+    $("#history").append(data);
   };
 });
