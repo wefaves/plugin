fdbf1e5a24c53a445b3503f6d894591e9659735b
youcef mouhoub <youcef.mouhoub@epitech.eu>
2016-12-08 20:42:10 +0100

    check difference :+1:

--- public/js/process.js
+++ public/js/process.js
@@ -31,22 +31,37 @@ $('#synchro').click(function() {
   sendBookmarks();
 });
 
+function  getDifference(local, dds)
+{
+  var difference = [];
+
+  jQuery.grep(dds, function(el) {
+          if (jQuery.inArray(el, local) == -1) difference.push(el);
+  });
+
+  return difference;
+}
+
 function  sendBookmarks()
 {
   var bookmarks = chrome.bookmarks.getTree(function(bookmarks) {
   var data = getBookmarks(bookmarks);
   console.log(data);
-      $.ajax({
+  var onserv = data.slice();
+  onserv.push({id: "id_test", title: "test difference", url: "www.youcef.com"});
+  data = getDifference(data, onserv);
+  console.log(data);
+      /*$.ajax({
         url: '',
         type: 'POST',
-        data: JSON.stringify(bookmarkTreeNodes),
+        data: JSON.stringify(data),
         contentType: 'application/json; charset=utf-8',
         dataType: 'json',
         async: false,
         success: function(msg) {
           console.log(msg);
         }
-      });
+      });*/
   });
 
 }
