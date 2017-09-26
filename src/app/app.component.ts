fb3364cf4ddc3ac655fdcf14a88e4945c9b9ad34
Margheriti <lilrom13@gmail.com>
2017-09-26 16:11:13 +0200

    add redirection when no token and add environment managment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import { Http } from '@angular/http';
 import { TokenService } from './services/token.service';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +9,23 @@ import { TokenService } from './services/token.service';
 
 export class AppComponent implements OnInit {
 
-  constructor(private http: Http, private tokenService: TokenService) { }
+  constructor(private tokenService: TokenService) { }
 
   ngOnInit() {
-    this.tokenService.getCookies("http://wefaves.com/", "token", (key) => {
+    this.tokenService.getCookies(environment.web_app_endpoint, "token", (key) => {
       this.tokenService.setToken(key);
+
+      if (!this.tokenService.token) {
+        window.open(environment.web_app_endpoint+'/#/account/login');
+      }
     });
   }
 
-  public open(event) {
-    window.open('http://dev.my.wefaves.com.s3-website.eu-central-1.amazonaws.com/#/');
+  public open() {
+    window.open(environment.web_app_endpoint);
   }
 
-  public settings(event) {
-    window.open('http://dev.my.wefaves.com.s3-website.eu-central-1.amazonaws.com/#/account');
+  public settings() {
+    window.open(environment.web_app_endpoint+'/#/account/profile');
   }
 }
